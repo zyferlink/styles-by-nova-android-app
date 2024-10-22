19a22a2802843a110ad3217f6092bb83e2d63929
NovaLogics <rajitha.shavinda@gmail.com>
2024-10-22 18:16:01 +0530

    feat [ ui ] : Update main screen

--- app/src/main/java/novalogics/android/styles/presentation/ui/home/HomeScreen.kt
+++ app/src/main/java/novalogics/android/styles/presentation/ui/home/HomeScreen.kt
@@ -1,2 +1,14 @@
 package novalogics.android.styles.presentation.ui.home
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun HomeScreen(
+    viewModel: HomeViewModel = hiltViewModel(),
+    contentPadding: PaddingValues = PaddingValues(0.dp)
+) {
+
+}
