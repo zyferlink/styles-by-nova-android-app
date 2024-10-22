fd7f5228e214d6c4da0702bc90135e4a03cd09d9
NovaLogics <rajitha.shavinda@gmail.com>
2024-10-22 18:03:38 +0530

    feat [ main-screen ] : Update main activity with composable screen

--- app/src/main/java/novalogics/android/styles/app/MainActivity.kt
+++ app/src/main/java/novalogics/android/styles/app/MainActivity.kt
@@ -1,49 +1,52 @@
 package novalogics.android.styles.app
 
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 import novalogics.android.styles.presentation.theme.StylesByNovaTheme
+import novalogics.android.styles.presentation.ui.mainscreen.StylesMainScreen
+import novalogics.android.styles.util.Constants
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //TODO : Only for testing
+        installSplashScreen().setKeepOnScreenCondition {
+            runBlocking { delay(1000) }
+            false
+        }
         enableEdgeToEdge()
         setContent {
             StylesByNovaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                StylesMainScreen()
             }
         }
     }
 }
 
+@Preview(
+    name = Constants.MODE_LIGHT,
+    showBackground = true,
+    uiMode = UI_MODE_NIGHT_NO
+)
+@Preview(
+    name = Constants.MODE_NIGHT,
+    showBackground = true,
+    uiMode = UI_MODE_NIGHT_YES
+)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
+fun AppPreview() {
     StylesByNovaTheme {
-        Greeting("Android")
+        StylesMainScreen()
     }
 }
