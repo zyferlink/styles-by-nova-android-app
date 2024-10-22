aa9b398b9b9215a9356fae75af0df6126d54aad7
NovaLogics <rajitha.shavinda@gmail.com>
2024-10-22 18:48:53 +0530

    feat [ ui ] : Completed Stage 1  Units for HomeScreenViewModel

--- app/src/main/java/novalogics/android/styles/presentation/ui/home/HomeViewModel.kt
+++ app/src/main/java/novalogics/android/styles/presentation/ui/home/HomeViewModel.kt
@@ -1,7 +1,13 @@
 package novalogics.android.styles.presentation.ui.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import novalogics.android.styles.data.repository.HomeRepositoryOffline
 import javax.inject.Inject
 
@@ -9,5 +15,31 @@ import javax.inject.Inject
 class HomeViewModel @Inject constructor(
     private val repositoryOffline: HomeRepositoryOffline
 ) : ViewModel() {
+    private val _uiState = MutableStateFlow(HomeUiState())
+    val uiState: StateFlow<HomeUiState> = _uiState
 
+
+    init {
+        loadDataOffline()
+    }
+
+    private fun loadDataOffline() {
+        _uiState.update { ui ->
+            ui.copy(
+                isLoading = true,
+                bannerData = repositoryOffline.getBannerUrls(),
+                eventData = repositoryOffline.getDemoEventsMen()
+            )
+        }
+
+        viewModelScope.launch {
+            delay(4000)
+            _uiState.update { it.copy(isLoading = false) }
+        }
+    }
+
+
+    fun handleError() {
+        _uiState.value = HomeUiState(error = "Something went wrong!")
+    }
 }
