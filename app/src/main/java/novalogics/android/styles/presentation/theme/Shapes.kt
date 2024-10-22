c7bdbb5ee8e9509bfa1f7ba02b4ddc26eb5b055c
NovaLogics <rajitha.shavinda@gmail.com>
2024-10-22 16:48:27 +0530

    feat [ res-shapes ] : Add Shapes data to the project

---
+++ app/src/main/java/novalogics/android/styles/presentation/theme/Shapes.kt
@@ -0,0 +1,16 @@
+package novalogics.android.styles.presentation.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(50.dp),
+
+    medium = RoundedCornerShape(
+        topStart = 0.dp,
+        topEnd = 24.dp,
+        bottomStart = 24.dp,
+        bottomEnd = 0.dp
+    )
+)
