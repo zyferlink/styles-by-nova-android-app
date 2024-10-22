e28d829eda73f4b95ac290845ce8e3abe90539d5
NovaLogics <rajitha.shavinda@gmail.com>
2024-10-22 18:00:01 +0530

    feat [ main-screen ] : Add main screen composable

---
+++ app/src/main/java/novalogics/android/styles/presentation/ui/mainscreen/StylesMainScreen.kt
@@ -0,0 +1,12 @@
+package novalogics.android.styles.presentation.ui.mainscreen
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun StylesMainScreen(
+    navController: NavHostController = rememberNavController()
+){
+
+}
