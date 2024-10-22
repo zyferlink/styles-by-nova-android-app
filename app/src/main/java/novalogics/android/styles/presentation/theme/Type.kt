8f7cdaead3f5ad9dc0257e56f6ca39a7f53e4382
NovaLogics <rajitha.shavinda@gmail.com>
2024-10-22 16:47:14 +0530

    feat [ res-typography ] : Add Typography data to the project

--- app/src/main/java/novalogics/android/styles/presentation/theme/Type.kt
+++ app/src/main/java/novalogics/android/styles/presentation/theme/Type.kt
@@ -2,19 +2,52 @@ package novalogics.android.styles.presentation.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import novalogics.android.styles.R
+
+val AbrilFatFace = FontFamily(
+    Font(R.font.abril_fatface_regular)
+)
+
+val Quicksand = FontFamily(
+    Font(R.font.quicksand_medium)
+)
+
+val Dalmais = FontFamily(
+    Font(R.font.dalmais)
+)
+
+val Montserrat = FontFamily(
+    Font(R.font.montserrat_regular),
+    Font(R.font.montserrat_bold, FontWeight.Bold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    displayLarge = TextStyle(
+        fontFamily = Dalmais,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 36.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = Quicksand,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
     )
+
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
