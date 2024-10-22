5ae59a648e6c091330dd4db53821f5a2347f7494
NovaLogics <rajitha.shavinda@gmail.com>
2024-10-22 18:18:17 +0530

    feat [ ui ] : Add search bar for HomeScreen

---
+++ app/src/main/java/novalogics/android/styles/presentation/ui/home/component/SearchBar.kt
@@ -0,0 +1,63 @@
+package novalogics.android.styles.presentation.ui.home.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import novalogics.android.styles.R
+
+@Composable
+fun SearchBar(
+    query: String,
+    onQueryChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TextField(
+        value = query,
+        onValueChange = onQueryChange,
+        modifier = modifier
+            .fillMaxWidth()
+            .heightIn(min = 40.dp, max = 54.dp)
+            .padding(
+                start = dimensionResource(id = R.dimen.padding_medium_16dp),
+                end = dimensionResource(id = R.dimen.padding_medium_16dp),
+                top = dimensionResource(id = R.dimen.padding_regular_8dp),
+                bottom = dimensionResource(id = R.dimen.padding_regular_8dp),
+            ),
+        placeholder = { Text(text = "Search...") },
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = "Search Icon"
+            )
+        },
+        singleLine = true,
+        shape = MaterialTheme.shapes.medium,
+        colors = TextFieldDefaults.colors(
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        )
+    )
+}
