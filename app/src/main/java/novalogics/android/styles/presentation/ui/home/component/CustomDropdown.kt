1af1342bfd21a57ea52a3c6ca5174ddff73dc0b6
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-03 14:56:06 +0530

    feat [ui-design] : Add CustomDropdown component

---
+++ app/src/main/java/novalogics/android/styles/presentation/ui/home/component/CustomDropdown.kt
@@ -0,0 +1,15 @@
+package novalogics.android.styles.presentation.ui.home.component
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
+
+@Composable
+fun CustomDropdown(
+    items: List<String>,
+    modifier: Modifier = Modifier,
+    shape: Shape = MaterialTheme.shapes.medium
+) {
+
+}
