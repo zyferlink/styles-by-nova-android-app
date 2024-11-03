463a452100679d24319308aab68722b5c9117739
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-03 14:58:44 +0530

    feat [ui-design] : Update CustomDropdown component

--- app/src/main/java/novalogics/android/styles/presentation/ui/home/component/CustomDropdown.kt
+++ app/src/main/java/novalogics/android/styles/presentation/ui/home/component/CustomDropdown.kt
@@ -1,15 +1,71 @@
 package novalogics.android.styles.presentation.ui.home.component
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.sp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CustomDropdown(
     items: List<String>,
     modifier: Modifier = Modifier,
     shape: Shape = MaterialTheme.shapes.medium
 ) {
+    var expanded by remember { mutableStateOf(false) }
+    var selectedItem by remember { mutableStateOf(items[0]) }
 
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = { expanded = !expanded },
+    ) {
+        BasicTextField(
+            value = selectedItem,
+            onValueChange = {},
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.LightGray, shape) // Custom shape for text field
+                .exposedDropdownSize()
+                .clickable { expanded = true },
+            textStyle = TextStyle(fontSize = 16.sp)
+        )
+
+        ExposedDropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier
+                .clip(shape) // Custom shape for dropdown menu
+                .background(Color.White)
+        ) {
+            items.forEach { item ->
+                DropdownMenuItem(
+                    onClick = {
+                        selectedItem = item
+                        expanded = false
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(Color.White, shape)
+                ) {
+                    Text(text = item, fontSize = 16.sp)
+                }
+            }
+        }
+    }
 }
