3eb952f672b2ef22822305fdf637999f2ff42c73
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-03 15:45:47 +0530

    feat [ui-design] : Update CustomDropdown component - stage 2

--- app/src/main/java/novalogics/android/styles/presentation/ui/home/component/CustomDropdown.kt
+++ app/src/main/java/novalogics/android/styles/presentation/ui/home/component/CustomDropdown.kt
@@ -1,94 +1,79 @@
 package novalogics.android.styles.presentation.ui.home.component
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.background
+
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.material.DropdownMenuItem
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.capitalize
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.res.painterResource
+import novalogics.android.styles.R
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CustomDropdown(
     items: List<String>,
     modifier: Modifier = Modifier,
     shape: Shape = MaterialTheme.shapes.medium
 ) {
-    var expanded by remember { mutableStateOf(false) }
-    var selectedItem by remember { mutableStateOf(items[0]) }
-    val focusRequester = remember { FocusRequester() }
+    val isDropDownExpanded = remember {
+        mutableStateOf(false)
+    }
 
+    val itemPosition = remember {
+        mutableStateOf(0)
+    }
 
-        LaunchedEffect(Unit) {
-            // Only request focus when the dropdown is expanded
-            if (expanded) {
-                focusRequester.requestFocus()
+    val usernames = listOf("Alexander", "Isabella", "Benjamin", "Sophia", "Christopher")
+
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Box {
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.clickable {
+                    isDropDownExpanded.value = true
+                }
+            ) {
+                Text(text = usernames[itemPosition.value])
+                Image(
+                    painter = painterResource(id = R.drawable.baseline_arrow_drop_down_24),
+                    contentDescription = "DropDown Icon"
+                )
             }
-        }
+            DropdownMenu(
+                expanded = isDropDownExpanded.value,
+                onDismissRequest = {
+                    isDropDownExpanded.value = false
+                }) {
 
-        ExposedDropdownMenuBox(
-            expanded = expanded,
-            onExpandedChange = { expanded = !expanded }
-        ) {
-
-
-        AnimatedVisibility(visible = expanded) {
-            BasicTextField(
-                value = selectedItem,
-                onValueChange = {},
-                modifier = modifier
-                    .clickable {
-                        expanded = true
-                        focusRequester.requestFocus()  // Request focus on click
-                    }
-                    .background(Color.LightGray, shape)
-                    .focusRequester(focusRequester)  // Attach the focus requester to the Text modifier
-
-            )
-
-        }
-
-
-        ExposedDropdownMenu(
-            expanded = expanded,
-            onDismissRequest = { expanded = false },
-            modifier = Modifier
-                .clip(shape)
-                .background(Color.White)
-        ) {
-            items.forEach { item ->
-                DropdownMenuItem(
-                    onClick = {
-                        selectedItem = item
-                        expanded = false
+                usernames.forEachIndexed { index, username ->
+                    DropdownMenuItem(
+                        text = { Text(text = username)
                     },
-                    modifier = Modifier.background(Color.White)
-                ) {
-                    Text(text = item, fontSize = 16.sp)
+                        onClick = {
+                            isDropDownExpanded.value = false
+                            itemPosition.value = index
+                    })
                 }
             }
         }
+
     }
 }
