d3132f225f873968822a397fef5d7e4b0797fecc
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-04 14:12:18 +0530

    build [gradle]: Add DI DataStoreModule

---
+++ app/src/main/java/novalogics/android/styles/di/modules/DataStoreModule.kt
@@ -0,0 +1,30 @@
+package novalogics.android.styles.di.modules
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStoreFile
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataStoreModule {
+
+    @Provides
+    @Singleton
+    fun provideDataStore(
+        @ApplicationContext context: Context
+    ): DataStore<Preferences> {
+        return PreferenceDataStoreFactory.create(
+            produceFile = { context.preferencesDataStoreFile("app_preferences") }
+        )
+    }
+
+
+}
