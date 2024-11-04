78bf65a85a53f850f9f711b89a5872047534ff18
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-04 20:57:43 +0530

    feat [ui]: Update default values

--- app/src/main/java/novalogics/android/styles/ui/screens/home/HomeUiState.kt
+++ app/src/main/java/novalogics/android/styles/ui/screens/home/HomeUiState.kt
@@ -3,15 +3,14 @@ package novalogics.android.styles.ui.screens.home
 import novalogics.android.styles.data.model.home.EventEntity
 import novalogics.android.styles.data.type.FashionCategory
 
-private const val DEFAULT_LOADING_MESSAGE = "Loading..."
-private const val DEFAULT_SEARCH_VALUE = ""
+private const val DEFAULT_VALUE = ""
 
 data class HomeUiState(
     val isLoading: Boolean = false,
-    val loadingMessage: String = DEFAULT_LOADING_MESSAGE,
+    val loadingMessage: String = DEFAULT_VALUE,
     val bannerItems: List<String> = emptyList(),
     val eventCategories: List<EventEntity> = emptyList(),
     val fashionCategory: FashionCategory = FashionCategory.DEFAULT,
-    val searchFieldValue: String = DEFAULT_SEARCH_VALUE,
+    val searchFieldValue: String = DEFAULT_VALUE,
     val error: String? = null
 )
